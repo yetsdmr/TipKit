4b48f47e124b3007067fb31997bc49a54ecfb095
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-15 16:06:58 +0300

    Initial Commit

---
+++ TipKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipKit
+//
+//  Created by Yunus Emre Taşdemir on 15.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TipKit/TipKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKitApp.swift
+//  TipKit
+//
+//  Created by Yunus Emre Taşdemir on 15.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TipKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
