6c53eaeaf45c02da299140bd324f9e09d1bc0328
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-17 13:53:51 +0300

    Added tip rules and options.

--- TipKit/ContentView.swift
+++ TipKit/ContentView.swift
@@ -13,25 +13,38 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List {
-                
+                NavigationLink("iJustine") {
+                    VStack {
+                        
+                    }
+                    .navigationTitle("iJustine")
+                    .onAppear(perform: {
+                        FavouriteTip.numberofTimesVisited.donate()
+                    })
+                    .toolbar(content: {
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Button {
+                                //print(favouriteTip.status, FavouriteTip.showTip)
+                                print(favouriteTip.status)
+                                //FavouriteTip.showTip = true
+                            } label: {
+                                Image(systemName: "star")
+                            }
+                            .popoverTip(favouriteTip,arrowEdge: .top)
+
+                        }
+                    })
+                }
             }
             .navigationTitle ("Messages")
-            .toolbar (content: {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "star")
-                    }
-                    .popoverTip(favouriteTip,arrowEdge: .top)
-
-                }
-            })
         }
     }
 }
 
 struct FavouriteTip: Tip {
+    @Parameter
+    static var showTip: Bool = false
+    static var numberofTimesVisited: Event = Event(id: "dev. yetsdmr.tip.event.numberoftimesvisited")
     
     var title: Text {
         Text("Move Contact to Favourites")
@@ -43,6 +56,19 @@ struct FavouriteTip: Tip {
     
     var asset: Image? {
         Image(systemName: "star")
+    }
+    
+    var options: [TipOption] {
+        return [
+            Tips.MaxDisplayCount(3)
+        ]
+    }
+    
+    var rules: [Rule] {
+        return [
+            //#Rule(Self.$showTip) { $0 == true }
+            #Rule(Self.numberofTimesVisited) { $0.donations.count > 3 }
+        ]
     }
 }
 
